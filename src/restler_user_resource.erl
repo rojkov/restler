c3380e39fa7f6768353815b490d95d9ab0108ecf
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2015-11-08 17:50:01 +0200

    Implement deleting user resource

--- src/restler_user_resource.erl
+++ src/restler_user_resource.erl
@@ -2,6 +2,7 @@
 -export([
     init/1,
     allowed_methods/2,
+    delete_resource/2,
     content_types_accepted/2,
     put_user/2,
     to_html/2
@@ -17,7 +18,11 @@ init([]) ->
     %% {ok, undefined}.
 
 allowed_methods(ReqData, State) ->
-    {['GET', 'PUT'], ReqData, State}.
+    {['GET', 'PUT', 'DELETE'], ReqData, State}.
+
+delete_resource(ReqData, State) ->
+    error_logger:info_msg("Deleting resource. Path tokens:~p~n", [wrq:path_tokens(ReqData)]),
+    {delete_user(wrq:path_tokens(ReqData)), ReqData, State}.
 
 content_types_accepted(ReqData, State) ->
     {[{"application/json", put_user}], ReqData, State}.
@@ -37,6 +42,9 @@ put_user(ReqData, State) ->
                                   [wrq:path_tokens(ReqData), Body]),
             {store_user(wrq:path_tokens(ReqData), wrq:req_body(ReqData)), ReqData, State}
     end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Private
 
 verify_user_data(ReqData) ->
     try
@@ -64,4 +72,13 @@ store_user([Username | _], Document) ->
     true;
 store_user(_, _Document) ->
     error_logger:info_msg("Error. No user name~n"),
+    {halt, 409}.
+
+
+delete_user([Username | _]) ->
+    Pid = pooler:take_member(riak8087),
+    ok = riakc_pb_socket:delete(Pid, {<<"default">>, <<"users">>}, list_to_binary(Username)),
+    pooler:return_member(riak8087, Pid, ok),
+    true;
+delete_user(_) ->
     {halt, 409}.
